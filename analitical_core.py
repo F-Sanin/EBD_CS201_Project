64f30df45b0b46e3358f7184a9594d3e4286735f
F-Sanin <fedir.sanin.25@kse.org.ua>
2026-07-22 02:34:18 +0300

    Calculate 20-day and 50-day SMA

--- analitical_core.py
+++ analitical_core.py
@@ -5,3 +5,6 @@ import yfinance as yf
 
 ford_data = yf.download(tickers="F", start='2022-01-01', end='2026-01-01')
 print(ford_data.head())
+
+ford_data["SMA_short"] = ford_data["Close"].rolling(20).mean()
+ford_data["SMA_long"] = ford_data["Close"].rolling(50).mean()
