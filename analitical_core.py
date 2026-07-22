6272c652e4b432dae7f17ec7d6c50af207598106
dmytro-polivko <dmytro.polivko.25@kse.org.ua>
2026-07-22 11:04:48 +0300

    Calculate positions and buy/sell signals

--- analitical_core.py
+++ analitical_core.py
@@ -8,3 +8,8 @@ print(ford_data.head())
 
 ford_data["SMA_short"] = ford_data["Close"].rolling(20).mean()
 ford_data["SMA_long"] = ford_data["Close"].rolling(50).mean()
+
+
+ford_data["Position"] = np.where(ford_data["SMA_short"] > ford_data["SMA_long"], 1 ,0)
+ford_data["Signal"] = ford_data["Position"]- ford_data["Position"].shift(1)
+ford_data[ford_data["Signal"].isin([1,-1])]
