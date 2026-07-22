439e5fdf6c7bf462cdba31b932492f40be4c782d
F-Sanin <fedir.sanin.25@kse.org.ua>
2026-07-22 12:23:16 +0300

    Finding of strategy return and cumulative strategy for Ford Motor data

--- analitical_core.py
+++ analitical_core.py
@@ -9,7 +9,13 @@ print(ford_data.head())
 ford_data["SMA_short"] = ford_data["Close"].rolling(20).mean()
 ford_data["SMA_long"] = ford_data["Close"].rolling(50).mean()
 
-
 ford_data["Position"] = np.where(ford_data["SMA_short"] > ford_data["SMA_long"], 1 ,0)
 ford_data["Signal"] = ford_data["Position"]- ford_data["Position"].shift(1)
 ford_data[ford_data["Signal"].isin([1,-1])]
+
+ford_data["Market_Returns"] = ford_data["Close"].pct_change()
+ford_data["Strategy_Returns"] = ford_data["Market_Returns"]*ford_data["Position"].shift(1)
+ford_data["Cumulative_Strategy"] = (1+ford_data["Market_Returns"]).cumprod()
+
+
+
